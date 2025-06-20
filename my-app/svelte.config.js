df7b577fac301d074b5401ba67182737a2d52f50
Youssef Kanjaa <60183071+YSF-KNJ@users.noreply.github.com>
2025-06-20 05:55:27 +0100

    Update svelte.config.js

--- my-app/svelte.config.js
+++ my-app/svelte.config.js
@@ -1,6 +1,10 @@
-// svelte.config.js
 import adapter from '@sveltejs/adapter-netlify';
 
 export default {
-  kit: { adapter: adapter() }
+  kit: {
+    adapter: adapter({
+      edge: false,  // SSR via Node-based Netlify Functions (default)
+      split: false  // bundle SSR into one function; set to true for route-based split
+    })
+  }
 };
